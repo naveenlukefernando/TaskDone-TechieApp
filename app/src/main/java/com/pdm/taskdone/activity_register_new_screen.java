92a83b0b7207b9d274827c08af69422ec25cc24b
naveenlukefernando <naveenlukefernandog3@gmail.com>
2018-08-07 20:19:27 +0530

    #UI auto setting

---
+++ app/src/main/java/com/pdm/taskdone/activity_register_new_screen.java
@@ -0,0 +1,13 @@
+package com.pdm.taskdone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_register_new_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_new_screen);
+    }
+}
--- app/src/main/java/com/pdm/taskdone/sign_in_up_screen.java
+++ app/src/main/java/com/pdm/taskdone/sign_in_up_screen.java
@@ -1,7 +1,10 @@
 package com.pdm.taskdone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class sign_in_up_screen extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class sign_in_up_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_up_screen);
+
+        Button reg = (Button) findViewById(R.id.reg_btn);
+
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent reg_view = new Intent(view.getContext(),activity_register_new_screen.class);
+                startActivity(reg_view);
+            }
+        });
     }
 }
