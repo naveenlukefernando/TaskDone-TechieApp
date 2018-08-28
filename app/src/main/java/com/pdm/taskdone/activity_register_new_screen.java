15eb1825f130055fd19ef5cac921949506dd1976
naveenlukefernando <naveenlukefernandog3@gmail.com>
2018-08-28 11:44:08 +0530

    #back to start work 2018-08-28

--- app/src/main/java/com/pdm/taskdone/activity_register_new_screen.java
+++ app/src/main/java/com/pdm/taskdone/activity_register_new_screen.java
@@ -1,13 +1,213 @@
 package com.pdm.taskdone;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.pdm.taskdone.Model.User;
 
 public class activity_register_new_screen extends AppCompatActivity {
+
+
+    FirebaseAuth firebaseAuth;
+    FirebaseDatabase f_database;
+    DatabaseReference users;
+
+    private Button reg_done_btn;
+    private TextInputEditText name,email,phone,password,re_password;
+    private TextInputLayout name_lay,email_lay,phone_lay,password_lay,re_password_lay;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_new_screen);
+
+        //initilaize firebase
+        firebaseAuth = FirebaseAuth.getInstance();
+        f_database = FirebaseDatabase.getInstance();
+        users = f_database.getReference("Users");
+
+        //button
+         reg_done_btn= findViewById(R.id.reg_done_btn);
+
+         //ini text_input_fields
+        name = findViewById(R.id.first_name);
+        email = findViewById(R.id.e_mail_text);
+        phone = findViewById(R.id.text_phone_number);
+        password = findViewById(R.id.password_text);
+        re_password = findViewById(R.id.retype_password_textbox);
+
+        //ini text_lay
+                name_lay = findViewById(R.id.textInput_Layer_Name);
+                email_lay = findViewById(R.id.textInput_Layer_Email);
+                phone_lay = findViewById(R.id.textInput_Layer_Phone_number);
+                password_lay = findViewById(R.id.textInput_Layer_password);
+                re_password_lay = findViewById(R.id.Textinput_Layer_retype_password);
+
+
+
     }
+
+
+    public void  reg_done_click (View v){
+
+        if(!ValidateName () |!validateEmail () | !validatePhone () |  !validatePassword () | !validate_Re_Password ()) {
+            return;
+        }
+
+        Toast.makeText(activity_register_new_screen.this, "Loading..",
+                Toast.LENGTH_SHORT).show();
+
+
+            firebaseAuth.createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString())
+                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+
+                            //save user to firebase db
+                            User user = new User();
+                            user.setFname(name.getText().toString());
+                            user.setEmail(email.getText().toString());
+                            user.setPassword(password.getText().toString());
+                            user.setPhone_num(phone.getText().toString());
+
+                            Toast.makeText(activity_register_new_screen.this, "Loading..",
+                                    Toast.LENGTH_SHORT).show();
+
+                            //id email
+                            users.child(FirebaseAuth.getInstance().getUid()).setValue(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(activity_register_new_screen.this, "Registered Succesfully.",
+                                            Toast.LENGTH_LONG).show();
+
+                                    startActivity(new Intent(activity_register_new_screen.this,activity_Welcome.class));
+                                    finish();
+                                }
+                            })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Toast.makeText(activity_register_new_screen.this, "Registration Failed.",
+                                                    Toast.LENGTH_LONG).show();
+                                        }
+                                    });
+
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(activity_register_new_screen.this, "Registration Failed.",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+
+
+
+    }
+
+
+    //validation
+
+    private boolean ValidateName ()
+    {
+        String name = name_lay.getEditText().getText().toString().trim();
+
+        if(name.isEmpty()){
+            name_lay.setError("Field can't be Empty.");
+            return false;
+        }
+
+        {
+            name_lay.setError(null);
+            return true;
+        }
+
+    }
+
+    private boolean validateEmail ()
+    {
+        String email = email_lay.getEditText().getText().toString().trim();
+
+        if(email.isEmpty()){
+            email_lay.setError("Field can't be Empty.");
+            return false;
+        }
+
+        {
+            email_lay.setError(null);
+            return true;
+        }
+
+    }
+
+    private boolean validatePhone ()
+    {
+        String phone_num = phone_lay.getEditText().getText().toString().trim();
+
+        if(phone_num.isEmpty()){
+            phone_lay.setError("Field can't be Empty.");
+            return false;
+        }
+        else if(phone_num.length()< 10 | phone_num.length() > 10) {
+            phone_lay.setError("Phone number should be 6 characters.");
+            return false;
+        }
+        phone_lay.setError(null);
+        return true;
+    }
+
+    private boolean validatePassword ()
+    {
+        String password = password_lay.getEditText().getText().toString().trim();
+
+        if(password.isEmpty()){
+            password_lay.setError("Field can't be Empty.");
+            return false;
+        }
+        else if(password.length()< 6) {
+            password_lay.setError("Password should be 6 or more characters.");
+            return false;
+        }
+        password_lay.setError(null);
+        return true;
+    }
+
+    private boolean validate_Re_Password ()
+    {
+        String re_Password = re_password_lay.getEditText().getText().toString().trim();
+
+        if(re_Password.isEmpty()){
+            re_password_lay.setError("Field can't be Empty.");
+            return false;
+        }
+        else if(re_Password.length()< 6) {
+            re_password_lay.setError("Password should be 6 or more characters.");
+            return false;
+        }
+        re_password_lay.setError(null);
+        return true;
+    }
+
+
+
 }
--- app/src/main/java/com/pdm/taskdone/sign_in_up_screen.java
+++ app/src/main/java/com/pdm/taskdone/sign_in_up_screen.java
@@ -1,14 +1,23 @@
 package com.pdm.taskdone;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,7 +30,9 @@ import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 public class sign_in_up_screen extends AppCompatActivity {
 
     private Button register_btn,login_btn;
-    private TextInputLayout texrinputEmail, textInputPassword;
+    private  TextInputLayout text_layer_Email, text_layer_password;
+    private TextInputEditText textinput_edit_Email, textInput_edit_Password;
+    private ConstraintLayout rootLayout;
 
     FirebaseAuth firebaseAuth;
     FirebaseDatabase f_database;
@@ -41,81 +52,68 @@ public class sign_in_up_screen extends AppCompatActivity {
          register_btn = (Button) (findViewById(R.id.reg_btn));
          login_btn = (Button) (findViewById(R.id.log_btn));
 
-         texrinputEmail = findViewById(R.id.e_mail_text_input);
-         textInputPassword = findViewById(R.id.passwordLayout);
+         textinput_edit_Email = findViewById(R.id.e_mail_text);
+         textInput_edit_Password = findViewById(R.id.password_input);
 
+         text_layer_Email = findViewById(R.id.e_mail_text_input_layer);
+         text_layer_password = findViewById(R.id.passwordLayout);
 
-         register_btn.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View view) {
-                 showRegisterDialog();
-             }
-         });
-
-
-
-         //add new user
-        firebaseAuth.createUserWithEmailAndPassword(texrinputEmail.getEditText().getText().toString(),textInputPassword.getEditText().getText().toString())
-                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                    @Override
-                    public void onSuccess(AuthResult authResult) {
-
-                        //save user to firebase db
-                            User user = new User();
-                            user.setFname();
-                    }
-                });
 
 
 
     }
 
-    private void showRegisterDialog() {
 
-                AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-                dialog.setTitle("Register");
-                dialog.setMessage("Please use email to register.");
-
-        LayoutInflater inflater = LayoutInflater.from(this);
-         View register_layout = inflater.inflate(R.layout.activity_register_new_screen,null);
-
-         TextInputLayout Name = register_layout.findViewById(R.id.textInput_Name);
-        TextInputLayout phone_num = register_layout.findViewById(R.id.textInput_Phone_number);
-        TextInputLayout email = register_layout.findViewById(R.id.textInput_Email);
-        TextInputLayout password = register_layout.findViewById(R.id.textInput_password);
-        TextInputLayout re_type_password = register_layout.findViewById(R.id.Textinput_retype_password);
-
-        dialog.setView(register_layout);
-
-    }
-
-    // input validation
-    public void register_click (View v)
+    //  login_attepmt
+    public void log_click (View v)
     {
-
         if (!validateEmail() | !validatePassword()){
             return;
         }
 
+        Toast.makeText(sign_in_up_screen.this, "Loading..",
+                Toast.LENGTH_SHORT).show();
+
+        //Login
+
+        firebaseAuth.signInWithEmailAndPassword(textinput_edit_Email.getText().toString(),textInput_edit_Password.getText().toString())
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        startActivity(new Intent(sign_in_up_screen.this,activity_Welcome.class));
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+                Toast.makeText(sign_in_up_screen.this, "Login Failed. Check your credentials.",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 
+
+    public void register_click (View v)
+    {
+        startActivity(new Intent(sign_in_up_screen.this,activity_register_new_screen.class));
+        finish();
+
+    }
+
+
     private boolean validateEmail ()
     {
-            String email = texrinputEmail.getEditText().getText().toString().trim();
+            String email = text_layer_Email.getEditText().getText().toString().trim();
 
             if(email.isEmpty()){
-                texrinputEmail.setError("Field can't be Empty.");
+                text_layer_Email.setError("Field can't be Empty.");
                 return false;
                 }
 
-                else if(email.contains("@")){
-                    texrinputEmail.setError("@ not included. Please Check");
-                    return false;
-                }
-
             {
-                    texrinputEmail.setError(null);
+                    text_layer_Email.setError(null);
                     return true;
             }
 
@@ -124,17 +122,17 @@ public class sign_in_up_screen extends AppCompatActivity {
 
     private boolean validatePassword ()
     {
-        String password = textInputPassword.getEditText().getText().toString().trim();
+        String password = text_layer_password.getEditText().getText().toString().trim();
 
         if(password.isEmpty()){
-            textInputPassword .setError("Field can't be Empty.");
+            text_layer_password.setError("Field can't be Empty.");
             return false;
         }
         else if(password.length()< 6) {
-            textInputPassword.setError("Password should be 6 or more characters.");
+            text_layer_password.setError("Password should be 6 or more characters.");
             return false;
         }
-        textInputPassword.setError(null);
+        text_layer_password.setError(null);
         return true;
     }
 
